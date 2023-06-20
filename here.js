851fc29b917fc60b12bc6d807d3556ecef2f8747
Kolten ✔️ <kolten.pilar@gmail.com>
2023-06-20 19:43:53 -0400

    added seperate pages for css/js

--- here.js
+++ here.js
@@ -1,21 +1,22 @@
  // added an alert to show viewers my message
- alert('Thank you for viewing! I followed along YouTuber SuperSimpleDev before any formal classes with PerScholas. The only thing I did myself was the CSS Styling')
+        alert('Thank you for viewing! I followed along YouTuber SuperSimpleDev before any formal classes with PerScholas. The only thing I did myself was the CSS Styling')
      
- let calculation = localStorage.getItem('calculation') || '';
+      let calculation = localStorage.getItem('calculation') || '';
 
- // Display the calculation when the page first loads.
- displayCalculation();
+      // Display the calculation when the page first loads.
+      displayCalculation();
 
- function updateCalculation(value) {
-   calculation += value;
+      function updateCalculation(value) {
+        calculation += value;
 
-   // Display the calculation on the page
-   // instead of console.log
-   displayCalculation();
+        // Display the calculation on the page
+        // instead of console.log
+        displayCalculation();
 
-   localStorage.setItem('calculation', calculation);
- }
+        localStorage.setItem('calculation', calculation);
+      }
 
- function displayCalculation() {
-   document.querySelector('.js-calculation').innerHTML = calculation;
- }
+      function displayCalculation() {
+        document.querySelector('.js-calculation')
+          .innerHTML = calculation;
+      }
